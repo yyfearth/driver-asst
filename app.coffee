bffe38ee80e328cf9582e2e9635b36893aa6b91b
Wilson Young <yyfearth@gmail.com>
2011-11-17 01:10:50 -0800

    rewrite the map control

--- app.coffee
+++ app.coffee
@@ -1,179 +1,191 @@
-window.$get = (id) -> document.getElementById id
-#$find = (path) -> document.querySelector path
-#$findAll = (path) -> document.querySelectorAll path
-window.maps =
-	getCurPos: (callback) ->
-		navigator.geolocation.getCurrentPosition ((pos) ->
-			# get geo location
-			curlatlng = new google.maps.LatLng pos.coords.latitude, pos.coords.longitude
-			console.log 'curlatlng', curlatlng
-			maps.curlatlng = curlatlng
-			callback curlatlng
-		), (-> alert 'no geo location!') if navigator.geolocation?
-	home: null
-
-# ui helper
+window.app = # ns
+	back: () -> history.go -1
+# init screen
+location.hash = '#home'
 # add back and home button to every page except home
 $('[data-role="page"] [data-role="header"]:not(.ui-non-nav)').append $('[data-btn-role="back"],[data-btn-role="home"]')
 $('#search [data-btn-role="home"]').hide()
 # adjust ref height
-h = $(document.body).height()
-$('.map').height h * 0.35
-# end of ui helper
-
+map_el = $ '#map'
+map_el.add(document.querySelectorAll '.map').height Math.round document.body.clientHeight * 0.35
+# build shared map
+map = app.map = new google.maps.Map map_el[0],
+	zoom: 15
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+	navigationControl: true
+	navigationControlOptions: 
+		style: google.maps.NavigationControlStyle.SMALL
+svc = new google.maps.places.PlacesService map
+geocoder = new google.maps.Geocoder()
+# ext map
+(() -> # @ is map
+	@el = map_el;
+	# add traffic
+	trafficLayer = new google.maps.TrafficLayer()
+	trafficLayer.setMap @
+	$.extend @,
+		move: (id) =>
+			@el.detach()
+			$("##{id} .map").append @el
+			@ # end of move
+		setMarkers: (markers_cfg) =>
+			console.log 'markers', markers_cfg
+			if not markers_cfg
+				markers_cfg = null
+			else if not $.isArray markers_cfg
+				markers_cfg = [markers_cfg]
+			# clear old markers
+			@markers.forEach ((marker) => # whether markers is null
+				marker.setMap null
+				delete marker
+			) if @markers?
+			# set new markers
+			if markers_cfg? # not null
+				@markers = markers_cfg.map (cfg) ->
+					cfg.map = map # auto set map
+					new google.maps.Marker cfg # return
+			else @markers = null
+			@ # end of set markers
+		getCurPos: (auto, callback) => # default auto center
+			if not callback?
+				callback = auto
+				auto = true
+			navigator.geolocation.getCurrentPosition ((pos) =>
+				# get geo location
+				curlatlng = new google.maps.LatLng pos.coords.latitude, pos.coords.longitude
+				console.log 'curlatlng', curlatlng
+				app.curlatlng = curlatlng
+				if auto
+					# clear markders
+					@setMarkers null
+					# set center
+					@setCenter curlatlng
+					# get addr
+					geocoder.geocode latLng: curlatlng, (results, status) =>
+						if status is google.maps.GeocoderStatus.OK
+							callback.call @, curlatlng, results[0]?.formatted_address
+						else
+							alert "Geocoder failed due to: #{status}\n App terminated!"
+				else callback.call @ curlatlng
+			), (-> alert 'App cannot run without geo location!') if navigator.geolocation?
+			@ # end of get cur pos
+).call map # end of ext map
 # home page
 $('#home').bind
-	pagecreate: () ->
-		console.log 'home pagecreate'
-		# init nav api and home page
-		maps.getCurPos (curlatlng) ->
-			# init map with location in center
-			@map = maps.home = new google.maps.Map $get('home_map'),
-				zoom: 15
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-				navigationControl: true
-				navigationControlOptions: 
-					style: google.maps.NavigationControlStyle.SMALL
-			# add traffic
-			trafficLayer = new google.maps.TrafficLayer()
-			trafficLayer.setMap maps.home
-			@map.mark = (curlatlng) ->
-				maps.home.setCenter curlatlng
-				# add marker
-				marker = new google.maps.Marker
-					position: curlatlng,
-					map: maps.home
-				# get addr and show info window
-				geocoder = new google.maps.Geocoder()
-				geocoder.geocode latLng: curlatlng, (results, status) ->
-					if status is google.maps.GeocoderStatus.OK
-						# console.log 'geocode', results
-						$('#home_addr').text results[0].formatted_address if results[0]?
-					else
-						alert "Geocoder failed due to: #{status}"
-				@ # end of mark
-			@map.mark curlatlng #  maps.home maybe null when pageshow
-			@ # end of getCurPos
-		@ # end of pagecreate
 	pageshow: () ->
 		console.log 'home pageshow'
-		maps.getCurPos ((curlatlng) -> @map.mark curlatlng) if @map? and not @map.getCenter()?.equals curlatlng
+		# init nav api and home page
+		map.getCurPos (curlatlng, addr) ->
+			$('#home_addr').text addr # set addr info
+			@setZoom 15
+			@setMarkers position: curlatlng # set cur marker
+			@move 'home'
+		#maps.getCurPos ((curlatlng) -> @map.mark curlatlng) if @map? and not @map.getCenter()?.equals curlatlng
+		@ # end of home page show
 
 # init history
 try
 	console.log localStorage.custom_search_history
-	maps.history = JSON.parse localStorage.custom_search_history if localStorage.custom_search_history?
-	maps.history = [] if not $.isArray maps.history
+	app.history = JSON.parse localStorage.custom_search_history if localStorage.custom_search_history?
+	app.history = [] if not $.isArray app.history
 catch err
 	console.log err
-	maps.history = []
+	app.history = []
 finally
-	maps.history.refresh = () =>
+	app.history.refresh = () =>
 		$('#history_list li:gt(0)').remove() # remove all except history_list_header
-		if maps.history.length
-			$('#history_list_header').after maps.history.map((item) ->"<li><a href=\"#result\" data-btn-role=\"search\">#{item}</a></li>").join ''
+		if app.history.length
+			$('#history_list_header').after app.history.map((item) ->"<li><a href=\"#result\" data-btn-role=\"search\">#{item}</a></li>").join ''
 		else
 			$('#history_list_header').after '<li data-role="list-divider" class="ui-body-c list-none">(None)</li>'
 
 # search menu page
-# $('#search').bind pageshow: () -> @map.keyword = null if @map? # clear maps.search_keyword
+# $('#search').bind pageshow: () -> @map.keyword = null if @map? # clear app.search_keyword
 # bind buttons and menus
 $('[data-btn-role="search"]').live vclick: () ->
-	maps.search_keyword = $(@).text()
-	console.log 'vclick', maps.search_keyword
+	app.search_keyword = $(@).text()
+	console.log 'vclick', app.search_keyword
 	@ # end of vclick
 # custom search
 $('#search_history').bind
 	pagecreate: () ->
 		@created = true
-		maps.history.refresh() # for the 1st show
+		app.history.refresh() # for the 1st show
 $('#search_history').bind 'pageshow pagebeforeshow', () -> $('#history_list').listview 'refresh' if @created
 $('#custom_search_form').submit () ->
 	input = $('#input_search')
 	keyword = $.trim input.val()
 	if keyword
-		maps.search_keyword = keyword
+		app.search_keyword = keyword
 		$.mobile.changePage '#result'
-		console.log 'vclick', maps.history.search_keyword
+		console.log 'vclick', app.history.search_keyword
 	else input.focus().val ''
 	false # end of submit
 # save history
 window.onbeforeunload = () ->
-	localStorage.custom_search_history = JSON.stringify maps.history
+	localStorage.custom_search_history = JSON.stringify app.history
 	return #"Sure to leave Knight Rider?"
 
 # result page
 $('#result').bind
-	pagecreate: () ->
-		maps.getCurPos (curlatlng) =>
-			@map = maps.result = new google.maps.Map $get('result_map'),
-				zoom: 12
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-				navigationControl: true
-				navigationControlOptions: 
-					style: google.maps.NavigationControlStyle.SMALL
-				center: curlatlng
-			@map.keyword = null # init maps.result.keyword
-			@map.search = (curlatlng) ->
-				console.log 'search for', maps.search_keyword
-				return if not maps.search_keyword
-				maps.result.keyword = maps.search_keyword
-				maps.result.setCenter maps.curlatlng
-				# clear old results
-				result_list = $('#result_list').empty()
-				# clear old markers
-				maps.result.markers.forEach ((marker) -> marker.setMap null) if maps.result.markers?
-				# search result
-				svc = new google.maps.places.PlacesService maps.result
-				svc.search
-					location: curlatlng
-					radius: 5000
-					keyword: maps.search_keyword
-					#types: ['store']
-					(results, status) -> # search callback
-						if status is google.maps.places.PlacesServiceStatus.OK
-							$('#result_addr').text "#{maps.search_keyword} (#{results.length})"
-							result_list.height h - result_list.offset().top
-							console.log 'search result', results
-							# sort results
-							results.forEach (r) ->
-								r = r.geometry
-								dlng = r.location.lng() - curlatlng.lng()
-								dlat = r.location.lat() - curlatlng.lat()
-								r.dist = Math.pow(dlng, 2) + Math.pow(dlat, 2)
-							results.sort (a, b) -> a.geometry.dist - b.geometry.dist
-							results = results.slice 0, 25 # only A-Z
-							# add to list in order
-							result_list.append results.map((result, i) ->
-								result.seq = String.fromCharCode 65 + i # from A
-								"<li><a href=\"#detail\" data-btn-role=\"result\" id=\"\">
+	pageshow: () ->
+		console.log 'search for', app.search_keyword
+		if not app.search_keyword
+			app.back(); return
+		#@map.keyword = null # init app.result.keyword
+		#app.result.keyword = app.search_keyword
+		# clear old results
+		result_list = $('#result_list').empty()
+		# create new results
+		map.getCurPos (curlatlng, addr) ->
+			@setZoom 12
+			@move 'result'
+			# search result
+			svc.search
+				location: curlatlng
+				radius: 5000
+				keyword: app.search_keyword
+				#types: ['store']
+				(results, status) -> # search callback
+					if status is google.maps.places.PlacesServiceStatus.OK
+						$('#result_addr').text "#{app.search_keyword} (#{results.length})"
+						result_list.height document.body.clientHeight - result_list.offset().top
+						console.log 'search result', results
+						# sort results
+						results.forEach (r) ->
+							r = r.geometry
+							dlng = r.location.lng() - curlatlng.lng()
+							dlat = r.location.lat() - curlatlng.lat()
+							r.dist = Math.pow(dlng, 2) + Math.pow(dlat, 2)
+						results.sort (a, b) -> a.geometry.dist - b.geometry.dist
+						results = results.slice 0, 25 # only A-Z
+						# add to list in order
+						result_list.append results.map((result, i) ->
+							result.seq = String.fromCharCode 65 + i # from A
+							"<li><a href=\"#detail\" data-btn-role=\"result\" id=\"\">
 <div style=\"float:left\">#{result.seq}</div><img src=\"#{result.icon}\" class=\"ui-li-icon\">
 <h3 class=\"ui-li-heading\">#{result.name}</h3><p class=\"ui-li-desc\">#{result.vicinity}</p></li>"
-							).join ''
-							# show marking in rev order
-							maps.result.markers = results.reverse().map (result) ->
-								# console.log result.seq
-								new google.maps.Marker
-									position: result.geometry.location
-									icon: "https://www.google.com/mapfiles/marker#{result.seq}.png"
-									map: maps.result
-							result_list.listview 'refresh' # end of if OK
-							# add marker
-							maps.result.markers.push new google.maps.Marker
-								position: curlatlng,
-								map: maps.result
-								icon: 'https://www.google.com/mapfiles/arrow.png'
-							# save history
-							if maps.history[0] isnt maps.search_keyword
-								maps.history.unshift maps.search_keyword
-								maps.history.refresh() # refresh list
-						@ # end of search callback
-				@ # end of search
-			@map.search curlatlng
-		@ # end of pagecreate
-	pagebeforeshow: () ->
-		$.mobile.changePage '#search' if not maps.search_keyword
-		maps.getCurPos ((curlatlng) -> maps.result.search curlatlng) if @map? and ((not @map.getCenter()?.equals maps.curlatlng) or (maps.result.keyword isnt maps.search_keyword))
-		@ # end of pageshow
+						).join ''
+						# show marking in rev order
+						markers = results.reverse().map (result) ->
+							position: result.geometry.location
+							icon: "https://www.google.com/mapfiles/marker#{result.seq}.png"
+						result_list.listview 'refresh' # end of if OK
+						# add marker
+						markers.push
+							position: curlatlng,
+							icon: 'https://www.google.com/mapfiles/arrow.png'
+						# set markers to map
+						map.setMarkers markers
+						# save history
+						if app.history[0] isnt app.search_keyword
+							app.history.unshift app.search_keyword
+							app.history.refresh() # refresh list
+					@ # end of search callback
+		@ # end of result page show
+	#pagebeforeshow: () ->
+		#$.mobile.changePage '#search' if not app.search_keyword
+		#app.getCurPos ((curlatlng) -> app.result.search curlatlng) if @map? and ((not @map.getCenter()?.equals app.curlatlng) or (maps.result.keyword isnt app.search_keyword))
+		#@ # end of pageshow
 
 console.log 1
--- app.js
+++ app.js
@@ -1,96 +1,125 @@
 (function() {
-  var h;
+  var geocoder, map, map_el, svc;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-  window.$get = function(id) {
-    return document.getElementById(id);
+  window.app = {
+    back: function() {
+      return history.go(-1);
+    }
   };
-  window.maps = {
-    getCurPos: function(callback) {
-      if (navigator.geolocation != null) {
-        return navigator.geolocation.getCurrentPosition((function(pos) {
-          var curlatlng;
-          curlatlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-          console.log('curlatlng', curlatlng);
-          maps.curlatlng = curlatlng;
-          return callback(curlatlng);
-        }), (function() {
-          return alert('no geo location!');
-        }));
-      }
-    },
-    home: null
-  };
+  location.hash = '#home';
   $('[data-role="page"] [data-role="header"]:not(.ui-non-nav)').append($('[data-btn-role="back"],[data-btn-role="home"]'));
   $('#search [data-btn-role="home"]').hide();
-  h = $(document.body).height();
-  $('.map').height(h * 0.35);
-  $('#home').bind({
-    pagecreate: function() {
-      console.log('home pagecreate');
-      maps.getCurPos(function(curlatlng) {
-        var trafficLayer;
-        this.map = maps.home = new google.maps.Map($get('home_map'), {
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          navigationControl: true,
-          navigationControlOptions: {
-            style: google.maps.NavigationControlStyle.SMALL
-          }
-        });
-        trafficLayer = new google.maps.TrafficLayer();
-        trafficLayer.setMap(maps.home);
-        this.map.mark = function(curlatlng) {
-          var geocoder, marker;
-          maps.home.setCenter(curlatlng);
-          marker = new google.maps.Marker({
-            position: curlatlng,
-            map: maps.home
-          });
-          geocoder = new google.maps.Geocoder();
-          geocoder.geocode({
-            latLng: curlatlng
-          }, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-              if (results[0] != null) {
-                return $('#home_addr').text(results[0].formatted_address);
-              }
-            } else {
-              return alert("Geocoder failed due to: " + status);
-            }
-          });
-          return this;
-        };
-        this.map.mark(curlatlng);
+  map_el = $('#map');
+  map_el.add(document.querySelectorAll('.map')).height(Math.round(document.body.clientHeight * 0.35));
+  map = app.map = new google.maps.Map(map_el[0], {
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    navigationControl: true,
+    navigationControlOptions: {
+      style: google.maps.NavigationControlStyle.SMALL
+    }
+  });
+  svc = new google.maps.places.PlacesService(map);
+  geocoder = new google.maps.Geocoder();
+  (function() {
+    var trafficLayer;
+    this.el = map_el;
+    trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(this);
+    return $.extend(this, {
+      move: __bind(function(id) {
+        this.el.detach();
+        $("#" + id + " .map").append(this.el);
         return this;
+      }, this),
+      setMarkers: __bind(function(markers_cfg) {
+        console.log('markers', markers_cfg);
+        if (!markers_cfg) {
+          markers_cfg = null;
+        } else if (!$.isArray(markers_cfg)) {
+          markers_cfg = [markers_cfg];
+        }
+        if (this.markers != null) {
+          this.markers.forEach((__bind(function(marker) {
+            marker.setMap(null);
+            return delete marker;
+          }, this)));
+        }
+        if (markers_cfg != null) {
+          this.markers = markers_cfg.map(function(cfg) {
+            cfg.map = map;
+            return new google.maps.Marker(cfg);
+          });
+        } else {
+          this.markers = null;
+        }
+        return this;
+      }, this),
+      getCurPos: __bind(function(auto, callback) {
+        if (!(callback != null)) {
+          callback = auto;
+          auto = true;
+        }
+        if (navigator.geolocation != null) {
+          navigator.geolocation.getCurrentPosition((__bind(function(pos) {
+            var curlatlng;
+            curlatlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+            console.log('curlatlng', curlatlng);
+            app.curlatlng = curlatlng;
+            if (auto) {
+              this.setMarkers(null);
+              this.setCenter(curlatlng);
+              return geocoder.geocode({
+                latLng: curlatlng
+              }, __bind(function(results, status) {
+                var _ref;
+                if (status === google.maps.GeocoderStatus.OK) {
+                  return callback.call(this, curlatlng, (_ref = results[0]) != null ? _ref.formatted_address : void 0);
+                } else {
+                  return alert("Geocoder failed due to: " + status + "\n App terminated!");
+                }
+              }, this));
+            } else {
+              return callback.call(this(curlatlng));
+            }
+          }, this)), (function() {
+            return alert('App cannot run without geo location!');
+          }));
+        }
+        return this;
+      }, this)
+    });
+  }).call(map);
+  $('#home').bind({
+    pageshow: function() {
+      console.log('home pageshow');
+      map.getCurPos(function(curlatlng, addr) {
+        $('#home_addr').text(addr);
+        this.setZoom(15);
+        this.setMarkers({
+          position: curlatlng
+        });
+        return this.move('home');
       });
       return this;
-    },
-    pageshow: function() {
-      var _ref;
-      console.log('home pageshow');
-      if ((this.map != null) && !((_ref = this.map.getCenter()) != null ? _ref.equals(curlatlng) : void 0)) {
-        return maps.getCurPos((function(curlatlng) {
-          return this.map.mark(curlatlng);
-        }));
-      }
     }
   });
   try {
     console.log(localStorage.custom_search_history);
     if (localStorage.custom_search_history != null) {
-      maps.history = JSON.parse(localStorage.custom_search_history);
+      app.history = JSON.parse(localStorage.custom_search_history);
     }
-    if (!$.isArray(maps.history)) {
-      maps.history = [];
+    if (!$.isArray(app.history)) {
+      app.history = [];
     }
   } catch (err) {
     console.log(err);
-    maps.history = [];
+    app.history = [];
   } finally {
-    maps.history.refresh = __bind(function() {
+    app.history.refresh = __bind(function() {
       $('#history_list li:gt(0)').remove();
-      if (maps.history.length) {
-        return $('#history_list_header').after(maps.history.map(function(item) {
+      if (app.history.length) {
+        return $('#history_list_header').after(app.history.map(function(item) {
           return "<li><a href=\"#result\" data-btn-role=\"search\">" + item + "</a></li>";
         }).join(''));
       } else {
@@ -100,15 +129,15 @@
   }
   $('[data-btn-role="search"]').live({
     vclick: function() {
-      maps.search_keyword = $(this).text();
-      console.log('vclick', maps.search_keyword);
+      app.search_keyword = $(this).text();
+      console.log('vclick', app.search_keyword);
       return this;
     }
   });
   $('#search_history').bind({
     pagecreate: function() {
       this.created = true;
-      return maps.history.refresh();
+      return app.history.refresh();
     }
   });
   $('#search_history').bind('pageshow pagebeforeshow', function() {
@@ -121,105 +150,74 @@
     input = $('#input_search');
     keyword = $.trim(input.val());
     if (keyword) {
-      maps.search_keyword = keyword;
+      app.search_keyword = keyword;
       $.mobile.changePage('#result');
-      console.log('vclick', maps.history.search_keyword);
+      console.log('vclick', app.history.search_keyword);
     } else {
       input.focus().val('');
     }
     return false;
   });
   window.onbeforeunload = function() {
-    localStorage.custom_search_history = JSON.stringify(maps.history);
+    localStorage.custom_search_history = JSON.stringify(app.history);
   };
   $('#result').bind({
-    pagecreate: function() {
-      maps.getCurPos(__bind(function(curlatlng) {
-        this.map = maps.result = new google.maps.Map($get('result_map'), {
-          zoom: 12,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          navigationControl: true,
-          navigationControlOptions: {
-            style: google.maps.NavigationControlStyle.SMALL
-          },
-          center: curlatlng
-        });
-        this.map.keyword = null;
-        this.map.search = function(curlatlng) {
-          var result_list, svc;
-          console.log('search for', maps.search_keyword);
-          if (!maps.search_keyword) {
-            return;
-          }
-          maps.result.keyword = maps.search_keyword;
-          maps.result.setCenter(maps.curlatlng);
-          result_list = $('#result_list').empty();
-          if (maps.result.markers != null) {
-            maps.result.markers.forEach((function(marker) {
-              return marker.setMap(null);
-            }));
-          }
-          svc = new google.maps.places.PlacesService(maps.result);
-          svc.search({
-            location: curlatlng,
-            radius: 5000,
-            keyword: maps.search_keyword
-          }, function(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-              $('#result_addr').text("" + maps.search_keyword + " (" + results.length + ")");
-              result_list.height(h - result_list.offset().top);
-              console.log('search result', results);
-              results.forEach(function(r) {
-                var dlat, dlng;
-                r = r.geometry;
-                dlng = r.location.lng() - curlatlng.lng();
-                dlat = r.location.lat() - curlatlng.lat();
-                return r.dist = Math.pow(dlng, 2) + Math.pow(dlat, 2);
-              });
-              results.sort(function(a, b) {
-                return a.geometry.dist - b.geometry.dist;
-              });
-              results = results.slice(0, 25);
-              result_list.append(results.map(function(result, i) {
-                result.seq = String.fromCharCode(65 + i);
-                return "<li><a href=\"#detail\" data-btn-role=\"result\" id=\"\"><div style=\"float:left\">" + result.seq + "</div><img src=\"" + result.icon + "\" class=\"ui-li-icon\"><h3 class=\"ui-li-heading\">" + result.name + "</h3><p class=\"ui-li-desc\">" + result.vicinity + "</p></li>";
-              }).join(''));
-              maps.result.markers = results.reverse().map(function(result) {
-                return new google.maps.Marker({
-                  position: result.geometry.location,
-                  icon: "https://www.google.com/mapfiles/marker" + result.seq + ".png",
-                  map: maps.result
-                });
-              });
-              result_list.listview('refresh');
-              maps.result.markers.push(new google.maps.Marker({
-                position: curlatlng,
-                map: maps.result,
-                icon: 'https://www.google.com/mapfiles/arrow.png'
-              }));
-              if (maps.history[0] !== maps.search_keyword) {
-                maps.history.unshift(maps.search_keyword);
-                maps.history.refresh();
-              }
+    pageshow: function() {
+      var result_list;
+      console.log('search for', app.search_keyword);
+      if (!app.search_keyword) {
+        app.back();
+        return;
+      }
+      result_list = $('#result_list').empty();
+      map.getCurPos(function(curlatlng, addr) {
+        this.setZoom(12);
+        this.move('result');
+        return svc.search({
+          location: curlatlng,
+          radius: 5000,
+          keyword: app.search_keyword
+        }, function(results, status) {
+          var markers;
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            $('#result_addr').text("" + app.search_keyword + " (" + results.length + ")");
+            result_list.height(document.body.clientHeight - result_list.offset().top);
+            console.log('search result', results);
+            results.forEach(function(r) {
+              var dlat, dlng;
+              r = r.geometry;
+              dlng = r.location.lng() - curlatlng.lng();
+              dlat = r.location.lat() - curlatlng.lat();
+              return r.dist = Math.pow(dlng, 2) + Math.pow(dlat, 2);
+            });
+            results.sort(function(a, b) {
+              return a.geometry.dist - b.geometry.dist;
+            });
+            results = results.slice(0, 25);
+            result_list.append(results.map(function(result, i) {
+              result.seq = String.fromCharCode(65 + i);
+              return "<li><a href=\"#detail\" data-btn-role=\"result\" id=\"\"><div style=\"float:left\">" + result.seq + "</div><img src=\"" + result.icon + "\" class=\"ui-li-icon\"><h3 class=\"ui-li-heading\">" + result.name + "</h3><p class=\"ui-li-desc\">" + result.vicinity + "</p></li>";
+            }).join(''));
+            markers = results.reverse().map(function(result) {
+              return {
+                position: result.geometry.location,
+                icon: "https://www.google.com/mapfiles/marker" + result.seq + ".png"
+              };
+            });
+            result_list.listview('refresh');
+            markers.push({
+              position: curlatlng,
+              icon: 'https://www.google.com/mapfiles/arrow.png'
+            });
+            map.setMarkers(markers);
+            if (app.history[0] !== app.search_keyword) {
+              app.history.unshift(app.search_keyword);
+              app.history.refresh();
             }
-            return this;
-          });
+          }
           return this;
-        };
-        return this.map.search(curlatlng);
-      }, this));
-      return this;
-    },
-    pagebeforeshow: function() {
-      var _ref;
-      if (!maps.search_keyword) {
-        $.mobile.changePage('#search');
-      }
-      if ((this.map != null) && ((!((_ref = this.map.getCenter()) != null ? _ref.equals(maps.curlatlng) : void 0)) || (maps.result.keyword !== maps.search_keyword))) {
-        maps.getCurPos((function(curlatlng) {
-          return maps.result.search(curlatlng);
-        }));
-      }
+        });
+      });
       return this;
     }
   });
